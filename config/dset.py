f7393a208d13398a05977be9cbb54844735e0c4a
FlamongoBird <jonathancalhoon@gmail.com>
2024-02-07 10:48:26 -0600

    .

--- config/dset.py
+++
@@ -1,8 +0,0 @@
-
-class DSET():
-    player = 5
-    treasure = 2
-    floor = 1
-    empty = 0
-    wall = 10
-    tunnel = 12
--- logger.py
+++
@@ -1,7 +0,0 @@
-
-with open("log.txt", "w") as file:
-	file.write("")
-
-def log(string):
-	with open("log.txt", "a+") as file:
-		file.write(string+"\n")
